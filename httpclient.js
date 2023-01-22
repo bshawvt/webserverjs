61365c10597c371f5f8c1648b41de52701b1e09a
Sasakwa-PC <bshawvt@gmail.com>
2023-01-22 18:10:05 -0500

    cleanup

--- httpclient.js
+++ httpclient.js
@@ -1,11 +1,8 @@
 var net = require("net");
 
 var client = new net.Socket();
-client.connect({host: "localhost", port: 8888}, function(a1, b1, c1) {
-	/*console.log(a1, b1, c1);
-	client.write("test", "UTF8", function(a2, b2, c2) {
-		console.log(a2, b2, c2);
-	});*/
+client.connect({host: "localhost", port: 8888}, function(a, b, c) {
+	console.log(1, b, c);
 });
 
 client.on("connect", function(a, b, c) {
@@ -15,7 +12,7 @@ client.on("close", function(a, b, c) {
 	console.log("on close:", a, b, c);
 });
 client.on("data", function(a, b, c) {
-	console.log("on data:", a.toString("utf8", 0, a.length), b, c);
+	console.log("on data:", a.toString("utf8", 0, a.length - 1), b, c);
 });
 client.on("end", function(a, b, c) {
 	console.log("on end:", a, b, c);
@@ -26,9 +23,6 @@ client.on("error", function(a, b, c) {
 client.on("ready", function(a, b, c) {
 	console.log("on ready:", a, b, c);
 	var url = "/index.html";
-	var msg = [`GET ${url} HTTP/1.1`, "Host: test", "\r\n"].join("\r\n");
-	//client.write("GET /index.html HTTP/1.1\r\nHost:1\r\n\r\n");
-	//console.log(msg);
-	client.write(msg);
-	//client.write("asda");
+	var headers = [`GET ${url} HTTP/1.1`, "Host: test", "\r\n"].join("\r\n");
+	client.write(headers);
 });
